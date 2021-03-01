8591bba4c3a2ea208e7e5c7bd6466255598c0c51
Arjun <62532677+vparjunmohan@users.noreply.github.com>
2021-03-01 20:06:57 +0530

    made changes to settings.py

--- todoDjangoRF/settings.py
+++ todoDjangoRF/settings.py
@@ -11,16 +11,24 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^o5i&df2ee$jnaen0612u&-m*-*y#-griouezurd0(q)r7y)y+'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
