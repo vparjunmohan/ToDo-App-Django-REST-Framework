a36000a1af9bdf6d0cd5b597fc0b6890802b7abe
Arjun <62532677+vparjunmohan@users.noreply.github.com>
2021-03-01 19:40:04 +0530

    implemented CRUD operations

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.apiOverview, name='api-overview')
+    path('', apiOverview, name='api-overview'),
+    path('task-list/', taskList, name='task-list'),
+    path('task-detail/<int:pk>/', taskDetail, name='task-detail'),
+    path('task-create/', taskCreate, name='task-create'),
+    path('task-update/<int:pk>/', taskUpdate, name='task-update'),
+    path('task-delete/<int:pk>/', taskDelete, name='task-delete'),
 ]
